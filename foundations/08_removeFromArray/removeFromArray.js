5d874a3a4407a7465b79a32cbff5e8ffface7885
Julio Rodriguez-Zambrano <228585521+rodBugo@users.noreply.github.com>
2026-04-28 13:45:16 -0600

    Add logic to remove multiple elements from array
    Use the rest parameter to pass multiple arguments
    Use filter method to remove elements

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...valuesToRemove) {
+    return array.filter(item => !valuesToRemove.includes(item));
 };
 
 // Do not edit below this line
